e4cc5a97ee3d8569682ef8c0e4c6cf70546710ad
Lorenzo <postaforu@gmail.com>
2015-01-11 10:41:52 +0100

    TableViewTest
    Alcune prove di UITableView

---
+++ MyTableViewControllerTestTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyTableViewControllerTestTableViewController.h
+//  TableViewTest
+//
+//  Created by Lorenzo on 10/01/15.
+//  Copyright (c) 2015 Lorenzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewControllerTestTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *planets;
+
+
+@end
